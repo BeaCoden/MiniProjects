207a192e5f1c09cc37697e700a5f4301709d35b9
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-10-26 22:04:47 +0200

    feat: added btn styling

--- TaxiBln/TaxiDriver/src/components/BtnReg.vue
+++ TaxiBln/TaxiDriver/src/components/BtnReg.vue
@@ -1,7 +1,7 @@
 <template>
   <router-link to="/register">
     <button
-      class="bg-secondary hover:bg-primary duration-300 font-sm text-fontColorNav py-2 px-4 rounded"
+      class="bg-secondary hover:bg-primary duration-300 text-xs text-fontColorNav py-2 px-4 rounded"
     >
       <i class="fas fa-user"></i>
       <span class="ml-2">Registrieren</span>
