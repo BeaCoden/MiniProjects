e23a63b7bff1330cb018e36f555ac4488894fae7
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-10-26 22:41:40 +0200

    feat: added tgl-fct navbar & styled navbar comp

--- TaxiBln/TaxiDriver/src/components/NavBar.vue
+++ TaxiBln/TaxiDriver/src/components/NavBar.vue
@@ -1,47 +1,62 @@
 <template>
   <div
-    class="bg-colorNav text-fontColorNav py-3.5 px-6 shadow justify-between items-center md:flex"
+    class="bg-colorNav text-fontColorNav py-3.5 px-6 shadow justify-between items-center"
   >
     <div class="flex items-center cursor-pointer">
-      <span class="text-fontColorNav text-xl mr-1">
-        <i class="fas fa-bars" />
+      <span class="text-fontColorNav hover:text-primary text-xl">
+        <i
+          id="navBtn"
+          class="fas fa-bars"
+          :class="[open ? 'fas fa-xmark' : 'fas fa-bars']"
+          @click="openMenu"
+        />
       </span>
-      <h1 class="text-xl">Taxi Billing</h1>
+      <h1 class="text-xl pl-5 font-semibold">Taxi Driver Billing</h1>
     </div>
-    <ul class="md:flex md:items-center">
-      <li v-for="link in links" :key="link.path" class="md:mx-4">
-        <router-link class="text-xl hover:text-red-500" :to="link.path">
+    <ul
+      class="w-1/2 absolute top-0 left-0 bg-colorNav pl-6 mt-14 ease-in-out transition-all duration-300 z-10"
+      style="height: calc(100vh - 3.5rem)"
+      :class="{ 'opacity-100': open, 'opacity-0': !open }"
+    >
+      <li v-for="link in links" :key="link.path" class="my-6">
+        <router-link class="text-sm hover:text-primary" :to="link.path">
           {{ link.name }}
         </router-link>
       </li>
-      <BtnReg />
-      <BtnLog />
+      <BtnLogIn />
+      <!-- <BtnReg /> -->
     </ul>
     <ul
       v-show="menuOpen"
-      class="md:hidden flex flex-col items-center justify-center text-xl text-fontColorNav"
+      class="flex flex-col items-center justify-center text-xl text-fontColorNav"
     ></ul>
   </div>
 </template>
 
 <script>
-import BtnLog from "./BtnLog.vue";
-import BtnReg from "./BtnReg.vue";
+import BtnLogIn from "./BtnLogIn.vue";
+// import BtnReg from "./BtnReg.vue";
+import { ref } from "vue";
 
 export default {
   name: "NavBar",
   components: {
-    BtnReg,
-    BtnLog,
+    BtnLogIn,
+    // BtnReg,
   },
   props: {},
   setup() {
+    let open = ref(false);
     let links = [
       { name: "Home", path: "/" },
-      { name: "About", path: "/about" },
       { name: "Dashboard", path: "/dashboard" },
     ];
-    return { links };
+
+    function openMenu() {
+      open.value = !open.value;
+    }
+
+    return { links, open, openMenu };
   },
   data() {
     return {
