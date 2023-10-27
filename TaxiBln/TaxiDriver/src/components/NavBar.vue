6b22044dbfac5702c288715442bc252c09497156
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-10-27 07:07:32 +0200

    feat: revised styling & toggleFct navbar

--- TaxiBln/TaxiDriver/src/components/NavBar.vue
+++ TaxiBln/TaxiDriver/src/components/NavBar.vue
@@ -7,18 +7,28 @@
         <i
           id="navBtn"
           class="fas fa-bars"
-          :class="[open ? 'fas fa-xmark' : 'fas fa-bars']"
-          @click="openMenu"
+          :class="[menuOpen ? 'fas fa-xmark' : 'fas fa-bars']"
+          @click="toggleMenu"
         />
       </span>
       <h1 class="text-xl pl-5 font-semibold">Taxi Driver Billing</h1>
     </div>
     <ul
-      class="w-1/2 absolute top-0 left-0 bg-colorNav pl-6 mt-14 ease-in-out transition-all duration-300 z-10"
+      id="navmenu"
+      class="w-1/2 absolute top-0 left-0 bg-colorNav pl-6 mt-14"
       style="height: calc(100vh - 3.5rem)"
-      :class="{ 'opacity-100': open, 'opacity-0': !open }"
+      :class="{
+        'opacity-100 animate__animated animate__fadeInLeft': menuOpen,
+        'opacity-0 animate__animated animate__fadeOutLeft': menuClosing,
+        hidden: !menuOpen,
+      }"
     >
-      <li v-for="link in links" :key="link.path" class="my-6">
+      <li
+        v-for="link in links"
+        :key="link.path"
+        class="my-6 animate__animated animate__fadeInLeft"
+        :class="{ 'animate__delay-0.1s': menuOpen }"
+      >
         <router-link class="text-sm hover:text-primary" :to="link.path">
           {{ link.name }}
         </router-link>
@@ -35,33 +45,41 @@
 
 <script>
 import BtnLogIn from "./BtnLogIn.vue";
-// import BtnReg from "./BtnReg.vue";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 export default {
   name: "NavBar",
   components: {
     BtnLogIn,
-    // BtnReg,
   },
   props: {},
   setup() {
-    let open = ref(false);
+    let menuOpen = ref(false);
+    let menuClosing = ref(false);
     let links = [
       { name: "Home", path: "/" },
       { name: "Dashboard", path: "/dashboard" },
     ];
 
-    function openMenu() {
-      open.value = !open.value;
+    function toggleMenu() {
+      if (menuOpen.value) {
+        menuClosing.value = true;
+        setTimeout(() => {
+          menuOpen.value = false;
+          menuClosing.value = false;
+        }, 500);
+      } else {
+        menuOpen.value = true;
+      }
     }
 
-    return { links, open, openMenu };
-  },
-  data() {
-    return {
-      menuOpen: false,
-    };
+    watch(menuOpen, () => {
+      if (menuOpen.value) {
+        menuClosing.value = false;
+      }
+    });
+
+    return { links, menuOpen, menuClosing, toggleMenu };
   },
 };
 </script>
