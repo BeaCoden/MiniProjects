9b03be18bf00f7f85bf93920f75ce13d79ab342d
Bea Pitzschke <bea.pitzschke.coden@gmail.com>
2023-10-30 13:08:16 +0100

    docs: updated setup files

--- TaxiBln/TaxiDriver/.eslintrc.js
+++ TaxiBln/TaxiDriver/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     parser: "@babel/eslint-parser",
   },
   rules: {
+    "linebreak-style": ["error", "unix"],
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
   },
